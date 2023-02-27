50a03f1e8103e18447b7a4e67ecbb0868446b92e
TheDracon <vicvarcas2007@gmail.com>
2023-02-27 21:11:26 +0100

    Started the chargiing activiiity

---
+++ app/src/main/java/com/example/irontextapp/CustomMediaPlayer.java
@@ -0,0 +1,13 @@
+package com.example.irontextapp;
+
+import android.media.MediaPlayer;
+
+public class CustomMediaPlayer extends MediaPlayer {
+
+	@Override
+	public void setOnCompletionListener(OnCompletionListener listener) {
+
+	}
+}
+
+
--- app/src/main/java/com/example/irontextapp/activities/MainActivity.java
+++ app/src/main/java/com/example/irontextapp/activities/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.irontextapp.activities;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.irontextapp.CustomMediaPlayer;
 import com.example.irontextapp.R;
 
 public class MainActivity extends AppCompatActivity {
-
+	VideoView video;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		if (getSupportActionBar() != null) getSupportActionBar().hide();
+//		String path = "android.resource://" + getPackageName() + "/" + R.raw.logo_anim;
+//		Uri uri = Uri.parse(path);
+//		video= findViewById(R.id.loadingAnimation);
+//		video.setVideoURI(uri);
+//		video.setOnCompletionListener((mp -> {
+//			video.start();
+//		}));
+//
+//		video.start();
+//		video.requestFocus();
 	}
 }
