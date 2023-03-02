8b93907cf9de745608f553cad76b3521c51120e7
TheDracon <vicvarcas2007@gmail.com>
2023-03-02 20:53:45 +0100

    Started RegisterActivity and finished

--- app/src/main/java/com/example/irontextapp/activities/RegisterActivity.java
+++ app/src/main/java/com/example/irontextapp/activities/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.example.irontextapp.activities;
 
+import android.content.Intent;
+import android.os.Looper;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.irontextapp.Main;
 import com.example.irontextapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -12,5 +18,65 @@ public class RegisterActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_register);
 
 		if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+		Button registerButton = findViewById(R.id.registerButton);
+		Button loginButton = findViewById(R.id.logInButton);
+
+		registerButton.setOnClickListener(view ->{
+			new Thread(()->{
+				Looper.prepare();
+				EditText emailInput = findViewById(R.id.emailInput);
+				EditText usernameInput = findViewById(R.id.nicknameInput);
+
+				EditText passwordInput = findViewById(R.id.passwordInput);
+				EditText repeatPasswordInput = findViewById(R.id.repeatPasswordInput);
+				String email = emailInput.getText().toString();
+				String password = passwordInput.getText().toString();
+				String repeatPassword = repeatPasswordInput.getText().toString();
+				String username = usernameInput.getText().toString();
+
+			if (!password.equals(repeatPassword)){
+					sendErrorMessage("Passwords are not the same", 1);
+				} else {
+
+					if (Main.getClient().isLoggedIn()) {
+						Main.getClient().closeConnection();
+						Main.getClient().startConnection();
+					}
+					int resultCode = Main.getClient().registerAcc(username,email, password);
+					System.out.println("RESULT CODE:" + resultCode);
+
+					switch(resultCode){
+						case 201:{
+							sendErrorMessage("Invalid password, password must contain at least 8 characters with numbers, capital and lowercase letters", 1);
+						}
+						case 301:{
+							sendErrorMessage("Invalid Email", 1);
+						}
+						case 302:{
+							sendErrorMessage("Email already registered", 1);
+						}
+						case 400:{
+							sendErrorMessage("Invalid username", 1);
+						}
+						case 401:{
+							sendErrorMessage("Invalid username", 1);
+						}
+						case 0:{
+							sendErrorMessage("Succsess", 1);
+							goToLogin();
+						}
+					}
+				}
+			}).start();
+		});
+		loginButton.setOnClickListener(view -> goToLogin());
+	}
+	public void sendErrorMessage(String text, int duration){
+		Toast.makeText(getApplicationContext(), text, duration).show();
+	}
+	public void goToLogin(){
+		Intent chatActivity = new Intent(this, LoginActivity.class);
+		startActivity(chatActivity);
 	}
 }
