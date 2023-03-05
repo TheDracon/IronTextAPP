f08554fb8490f57db064aceeab745c1f05daa0ce
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-05 21:16:27 +0100

    Added MessageErrorCodes

---
+++ app/src/main/java/com/example/irontextapp/MessageErrorCodes.java
@@ -0,0 +1,9 @@
+package com.example.irontextapp;
+
+public class MessageErrorCodes {
+
+    public static final int SUCCESS = 0;
+    public static final int INAPPROPRIATE_WORD = 1;
+    public static final int INVALID_CHARACTERS = 2;
+    public static final int MESSAGE_TOO_LONG = 3;
+}
