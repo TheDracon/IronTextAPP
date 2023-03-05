dd5dc23df501027d5811786820be72ed283ad3c4
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-05 21:15:46 +0100

    Added disconnected reqtype

--- app/src/main/java/com/example/irontextapp/RequestTypes.java
+++ app/src/main/java/com/example/irontextapp/RequestTypes.java
@@ -2,9 +2,11 @@ package com.example.irontextapp;
 
 public class RequestTypes {
 
-	// Require: THE MESSAGE
-	public static final int SEND_MESSAGE = 0;
+	public static final int DISCONNECTED = 3;
 
+	// Require: THE MESSAGE
+
+	public static final int SEND_MESSAGE = 0;
 
 	// Require: THE AMOUNT OF MESSAGES NEEDED
 	public static final int REQUEST_MESSAGES = 1;
